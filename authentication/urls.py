70f96b929b4fcb48087c0525d16c25d87a3c585a
Israt Jahan Mokta <israt.mukta06@gmail.com>
2023-05-25 21:37:45 +0600

    fixed static files problem

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 app_name = 'authentication'
 
 urlpatterns = [
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- biddaloy/settings.py
+++ biddaloy/settings.py
@@ -119,10 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-   
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
